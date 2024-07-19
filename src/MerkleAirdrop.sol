3d7db810a68f0484855ef75c96e596a316894b9d
Tully <tully.slattery@gmail.com>
2024-07-19 14:42:15 +0800

    created Airdrop contract and Merkle Script

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -2,10 +2,48 @@
 
 pragma solidity ^0.8.24;
 
-contract MerkleAirdrop {
-    //some list of addresses
-    //allow someone in the list to claim ERC-20 tokens
-    address[] claimers;
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-    
+contract MerkleAirdrop {
+    using SafeERC20 for IERC20;
+
+    error MerkleAirdrop__InvalidProof();
+    error MerkleAirdrop__AddressAlreadyClaimed();
+
+    address[] claimers;
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_airdropToken;
+    mapping(address claimer => bool claimed) s_hasClaimed;
+
+    event Claim(address account, uint256 amount);
+
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) {
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = airdropToken;
+    }
+
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        if (s_hasClaimed[account]) {
+            revert MerkleAirdrop__AddressAlreadyClaimed();
+        }
+
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount)))); //double hashing avoids collision - this is the current standard
+        if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
+            revert MerkleAirdrop__InvalidProof();
+        }
+        s_hasClaimed[account] = true;
+
+        emit Claim(account, amount);
+
+        i_airdropToken.safeTransfer(account, amount);
+    }
+
+    function getMerkleRoot() external view returns (bytes32) {
+        return i_merkleRoot;
+    }
+
+    function getAirdropToken() external view returns (IERC20) {
+        return i_airdropToken;
+    }
 }
